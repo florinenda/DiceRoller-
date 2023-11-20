cb23487d2ddf800c80fd7061216ab9b78bc3187b
hillarynda <hillarynda0@gmail.com>
2023-11-20 18:19:47 -0300

    Initial Commit

---
+++ DiceRoller/DiceRollerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DiceRollerApp.swift
+//  DiceRoller
+//
+//  Created by Apple Dev on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct DiceRollerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
